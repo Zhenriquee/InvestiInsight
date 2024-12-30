74737ad1a35a2b65fd4e720e823aee34269a39e0
Zhenriquee <luiz1919henrique@gmail.com>
2024-12-29 23:47:54 -0300

    4º commit descrição dos graficos
    Acrescentei descrições dos graficos.

--- app.py
+++ app.py
@@ -63,18 +63,86 @@ else:
             dataset_1_ano = fundo.evolucao_preco_grafico_linha_1_ano()
             grafico_linha.GraficoLinha.grafico_linha_evolucao_1_mes(dataset_1_ano)
 
+        
         dataframe_dividendos_transformado = fundo.evolucao_dividendos_grafico_barra()
         grafico_barra.Grafico_Barra.grafico_barra_comparacao_dividendos(dataframe_dividendos_transformado)
 
-        calculo_transformado_rsi = fundo.evolucao_preco_fii_grafico_linha_rsi()
-        grafico_linha.GraficoLinha.grafico_rsi(calculo_transformado_rsi, ticker)
+        graf1,graf2,graf3 = st.tabs(["📈 RSI","📈 MACD","📈 Bollinger"])
+        with graf1:
+            calculo_transformado_rsi = fundo.evolucao_preco_fii_grafico_linha_rsi()
+            grafico_linha.GraficoLinha.grafico_rsi(calculo_transformado_rsi, ticker)
+            with st.expander("Explicação Grafico RSI"):
+                st.markdown(''' O **RSI**  (**Relative Strength Index**) é um indicador de momentum que mede a velocidade e a mudança dos movimentos de preços. Ele ajuda a identificar condições de **sobrecompra** ou **sobrevenda** de um ativo.
 
-        calculo_transformado_macd, calculo_transformado_sinal_linha = fundo.evolucao_preco_fii_grafico_linha_macd()
-        grafico_linha.GraficoLinha.grafico_macd(calculo_transformado_macd, calculo_transformado_sinal_linha, ticker)
+### Cálculo:
+\[
+RSI = 100 - {100}/{1 + RS}
+\]
+Onde **RS** é a média dos fechamentos em alta dividida pela média dos fechamentos em baixa.
 
-        linha_central, close = fundo.evolucao_preco_fii_grafico_linha_bandas_de_bollinger_sma()
-        upper, lower = fundo.evolucao_preco_fii_grafico_linha_bandas_de_bollinger_upper_lower()
-        grafico_linha.GraficoLinha.grafico_bandas_de_bollinger(close, linha_central, upper, lower, ticker)
+### Análise:
+- **Sobrecompra (>70):** Pode indicar que o ativo está sobrecomprado e que o preço pode cair em breve.
+- **Sobrevenda (<30):** Pode indicar que o ativo está sobrevendido e que o preço pode subir em breve.
+- **Reversão em torno de 50:** Pode indicar uma possível mudança na direção do preço.
+
+
+'''
+                            )
+
+        with graf2:
+            calculo_transformado_macd, calculo_transformado_sinal_linha = fundo.evolucao_preco_fii_grafico_linha_macd()
+            grafico_linha.GraficoLinha.grafico_macd(calculo_transformado_macd, calculo_transformado_sinal_linha, ticker)
+            with st.expander("Explicação Grafico MACD"):
+                st.markdown('''O **MACD** (**Moving Average Convergence Divergence**) é um indicador de tendência que analisa a relação entre duas médias móveis exponenciais (EMAs) para identificar pontos de compra e venda.
+
+### Cálculo:
+\[
+MACD = EMA(12) - EMA(26)
+\]
+- **Linha de sinal:** EMA de 9 períodos do MACD.
+- **Histograma:** Diferença entre o MACD e a linha de sinal.
+
+### Análise:
+- **Cruzamento de linhas:**
+  - **MACD cruza acima da linha de sinal:** Sinal de compra.
+  - **MACD cruza abaixo da linha de sinal:** Sinal de venda.
+- **Divergência:**
+  - Se o preço faz novos máximos, mas o MACD não acompanha, pode ser sinal de fraqueza na tendência de alta (e vice-versa para a baixa).
+- **Histograma:**
+  - Indica a força da tendência. Um histograma crescente sinaliza tendência forte, enquanto um histograma decrescente pode indicar fraqueza.
+
+''')
+
+        with graf3:
+            linha_central, close = fundo.evolucao_preco_fii_grafico_linha_bandas_de_bollinger_sma()
+            upper, lower = fundo.evolucao_preco_fii_grafico_linha_bandas_de_bollinger_upper_lower()
+            grafico_linha.GraficoLinha.grafico_bandas_de_bollinger(close, linha_central, upper, lower, ticker)
+            with st.expander("Explicação Grafico Bandas de Bollinger"):
+                st.markdown('''As **Bandas de Bollinger** são compostas por uma média móvel simples (SMA) e duas bandas baseadas no desvio padrão, que ajudam a medir a volatilidade do mercado.
+
+### Cálculo:
+\[
+Banda Superior = SMA + (2 \times SD)
+\]
+\[
+Banda Inferior = SMA - (2 \times SD)
+\]
+
+### Análise:
+- **Preços tocando a banda superior:** Indicam possível condição de sobrecompra.
+- **Preços tocando a banda inferior:** Indicam possível condição de sobrevenda.
+- **Squeeze (estreitamento das bandas):** Pode sinalizar baixa volatilidade, antecipando um movimento significativo de preço.
+- **Expansão das bandas:** Indica alta volatilidade, sugerindo que o ativo pode estar em uma forte tendência.
+
+---
+
+### Resumo:
+- **RSI:** Mede sobrecompra (>70) e sobrevenda (<30) e pode antecipar reversões.
+- **MACD:** Identifica tendências e cruzamentos (compra ou venda).
+- **Bandas de Bollinger:** Indicam volatilidade e condições extremas de preço (sobrecompra/sobrevenda).
+
+''')
+
 
         # Gerar Resumo
         if st.button("Gerar Resumo do Relatório"):
